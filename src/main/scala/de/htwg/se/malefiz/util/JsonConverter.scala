54ebe2bdf71ac5ea94c1b36688905d94115a906a
jasafasa <f.steinhauser@hotmail.de>
2019-05-06 08:44:41 +0200

    Json finished

--- src/main/scala/de/htwg/se/malefiz/util/JsonConverter.scala
+++ src/main/scala/de/htwg/se/malefiz/util/JsonConverter.scala
@@ -1,7 +1,8 @@
 package de.htwg.se.malefiz.util
 
-import de.htwg.se.malefiz.model.gameboard.{BlockStone, PlayerStone}
+import de.htwg.se.malefiz.model.gameboard.{BlockStone, GameBoardInterface, PlayerStone}
 import de.htwg.se.malefiz.Malefiz.controller
+import de.htwg.se.malefiz.controller.State
 import play.api.libs.json._
 
 
@@ -9,28 +10,82 @@ object JsonConverter {
 
   def gameToJson(): JsObject = {
 
-      Json.obj(
-        "activePlayer" -> JsNumber(controller.activePlayer.color),
-        "diced" -> JsNumber(controller.diced),
-        "playerCount" -> JsNumber(controller.gameBoard.playerCount),
-        "blockStones" -> Json.toJson(
-          (0 to 16) flatMap (x =>
-            (0 to 13) filter (y => controller.gameBoard.board.contains((x, y)))
-              filter (y => controller.gameBoard.board((x, y)).stone.isDefined)
-              filter (y => controller.gameBoard.board((x, y)).stone.get.isInstanceOf[BlockStone])
-              map (y => Json.obj("x" -> JsNumber(x), "y" -> JsNumber(y))))),
-        "playerStones" -> Json.toJson(
-          (0 to 16) flatMap (x =>
-            (0 to 15) filter (y => controller.gameBoard.board.contains((x, y)))
-              filter (y => controller.gameBoard.board((x, y)).stone.isDefined)
-              filter (y => controller.gameBoard.board((x, y)).stone.get.isInstanceOf[PlayerStone])
-              map (y => Json.obj(
-              "x" -> JsNumber(x),
-              "y" -> JsNumber(y),
-              "startX" -> JsNumber(controller.gameBoard.board((x, y)).stone.get.asInstanceOf[PlayerStone].startX),
-              "startY" -> JsNumber(controller.gameBoard.board((x, y)).stone.get.asInstanceOf[PlayerStone].startY),
-              "playerColor" -> JsNumber(controller.gameBoard.board((x, y)).stone.get.asInstanceOf[PlayerStone].playerColor))))))
+    Json.obj(
+      "activePlayer" -> JsNumber(controller.activePlayer.color),
+      "diced" -> JsString(controller.diced.toString),
+      "message" -> JsString(message),
+      "rows" -> Json.toJson(
+        for {
+          y <- 0 to 15
+        } yield rowToJson(controller.gameBoard, y)))
 
+  }
+  def rowToJson(gameBoard: GameBoardInterface, y: Int): JsObject = {
+    Json.obj(
+      "rowNr" -> JsNumber(y),
+      "fields" -> Json.toJson(
+        for {
+          x <- 0 to 16
+        } yield fieldToJson(gameBoard, x, y)))
+  }
+  def fieldToJson(gameBoard: GameBoardInterface, x: Int, y: Int): JsObject = {
+    if (gameBoard.board.contains((x, y))) {
+      val field = gameBoard.board((x, y))
+      field.stone match {
+        case Some(stone: PlayerStone) =>
+          Json.obj(
+            "isFreeSpace" -> JsBoolean(false),
+            "x" -> JsNumber(x),
+            "y" -> JsNumber(y),
+            "sort" -> JsString(stone.playerColor.toString),
+            "avariable" -> JsBoolean(field.available))
+        case Some(_: BlockStone) =>
+          Json.obj(
+            "isFreeSpace" -> JsBoolean(false),
+            "x" -> JsNumber(x),
+            "y" -> JsNumber(y),
+            "sort" -> JsString("b"),
+            "avariable" -> JsBoolean(field.available))
+
+        case None =>
+          Json.obj(
+            "isFreeSpace" -> JsBoolean(false),
+            "x" -> JsNumber(x),
+            "y" -> JsNumber(y),
+            "sort" -> JsString("f"),
+            "avariable" -> JsBoolean(field.available))
+      }
+    } else {
+      Json.obj(
+        "isFreeSpace" -> JsBoolean(true),
+        "x" -> JsNumber(x),
+        "y" -> JsNumber(y),
+        "sort" -> JsString("f"),
+        "avariable" -> JsBoolean(false))
+    }
+  }
+  def message: String = {
+    controller.getState match {
+      case State.SetBlockStone =>
+        "Set a BlockStone"
+
+      case State.ChoosePlayerStone =>
+        "Chose one of your Stones"
+
+      case State.ChooseTarget =>
+        "Chose a Target Field"
+
+      case State.BeforeEndOfTurn =>
+        "End turn"
+
+      case State.SetPlayerCount =>
+        "Start a Game"
+
+      case State.PlayerWon =>
+        "Victory"
+
+      case _ => "Start a Game"
+    }
   }
 
 
