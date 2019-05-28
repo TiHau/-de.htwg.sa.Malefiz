4a5d18e14ef50f88fdf3103b17f3972969f7ee27
TiHau <tim.hauenstein97@gmx.de>
2019-05-28 14:09:23 +0200

    removed fileio

--- src/main/scala/de/htwg/se/malefiz/MalefizModule.scala
+++ src/main/scala/de/htwg/se/malefiz/MalefizModule.scala
@@ -3,7 +3,6 @@ package de.htwg.se.malefiz
 import com.google.inject.AbstractModule
 import com.google.inject.name.Names
 import de.htwg.se.malefiz.controller.{Controller, ControllerInterface}
-import de.htwg.se.malefiz.model.fileio.{FileIOInterface, _}
 import de.htwg.se.malefiz.model.gameboard.{GameBoard, GameBoardInterface}
 import net.codingwell.scalaguice.ScalaModule
 
@@ -15,7 +14,6 @@ class MalefizModule extends AbstractModule with ScalaModule {
     bindConstant().annotatedWith(Names.named("DefaultSize")).to(defaultSize)
     //bind[GameBoardInterface].to[GameBoard]
     bind[ControllerInterface].to[Controller]
-    bind[FileIOInterface].to[fileioJson.FileIO]
     bind[GameBoardInterface].annotatedWithName("default").toInstance(GameBoard(defaultSize))
     bind[GameBoardInterface].annotatedWithName("tiny").toInstance(GameBoard(tiny))
     bind[GameBoardInterface].annotatedWithName("small").toInstance(GameBoard(small))
